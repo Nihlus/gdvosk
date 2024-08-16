4dc95b67e33c53f64c0ca20244f2f52a6d7d7feb
Jarl Gullberg <Jarl.Gullberg@gmail.com>
2024-08-16 18:02:38 +0200

    Add template files.

---
+++ src/gdvosk.h
@@ -0,0 +1,13 @@
+// Copyright (C) 2024 Jarl Gullberg
+// SPDX-License-Identifier: MIT
+
+#ifndef GDVOSK_H
+#define GDVOSK_H
+
+#include <godot_cpp/core/class_db.hpp>
+
+void initialize_gdvosk_module(godot::ModuleInitializationLevel p_level);
+void uninitialize_gdvosk_module(godot::ModuleInitializationLevel p_level);
+
+#endif // GDVOSK_H
+
