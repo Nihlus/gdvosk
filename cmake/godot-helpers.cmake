e3bb5496467ee02f220f01da8da87d5d8df2f1ca
Jarl Gullberg <jarl.gullberg@gmail.com>
2024-08-24 14:28:21 +0200

    Support wasm builds.

--- cmake/godot-helpers.cmake
+++ cmake/godot-helpers.cmake
@@ -10,6 +10,11 @@ string(REGEX REPLACE "^x86$" x86_32 GODOT_ARCH ${GODOT_ARCH})
 string(REGEX REPLACE "^aarch64$" arm64 GODOT_ARCH ${GODOT_ARCH})
 string(REGEX REPLACE "^armv7-a$" arm32 GODOT_ARCH ${GODOT_ARCH})
 
+if ("${CMAKE_SYSTEM_NAME}" STREQUAL "Emscripten")
+    set(GODOT_SYSTEM_NAME "web")
+    set(GODOT_ARCH "wasm32")
+endif ()
+
 if (APPLE)
     set(GODOT_ARCH "universal")
 endif ()
