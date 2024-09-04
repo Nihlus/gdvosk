3e909b0b3f13473167842d3a84b16ab0efb895f5
Jarl Gullberg <jarl.gullberg@gmail.com>
2024-09-04 21:04:10 +0200

    Generalize toolchains.

--- cmake/toolchains/gcc-linux-gnu.cmake
+++ cmake/toolchains/gcc-linux-gnu.cmake
@@ -1,10 +1,17 @@
+option(TOOLCHAIN_PREFIX "The architecture prefix to use for the GCC toolchain." "x86_64-linux-gnu")
+
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR aarch64)
-set(TOOLCHAIN_PREFIX aarch64-linux-gnu)
 
 # cross compilers to use for C, C++
 set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
+
+execute_process(
+    COMMAND ${CMAKE_C_COMPILER} -dumpmachine
+    OUTPUT_VARIABLE GCC_DUMPMACHINE
+)
+
+string(REGEX REPLACE "-.+$" "" CMAKE_SYSTEM_PROCESSOR "${GCC_DUMPMACHINE}")
 
 # target environment on the build host system
 set(CMAKE_FIND_ROOT_PATH /usr/${TOOLCHAIN_PREFIX})
--- cmake/toolchains/gcc-w64-mingw.cmake
+++ cmake/toolchains/gcc-w64-mingw.cmake
@@ -1,19 +1,18 @@
-# Sample toolchain file for building for Windows from an Ubuntu Linux system.
-#
-# Typical usage:
-#    *) install cross compiler: `sudo apt-get install mingw-w64`
-#    *) cd build
-#    *) cmake -DCMAKE_TOOLCHAIN_FILE=~/mingw-w64-x86_64.cmake ..
-# This is free and unencumbered software released into the public domain.
+option(TOOLCHAIN_PREFIX "The architecture prefix to use for the GCC toolchain." "x86_64-w64-mingw32")
 
 set(CMAKE_SYSTEM_NAME Windows)
-set(CMAKE_SYSTEM_PROCESSOR x86_64)
-set(TOOLCHAIN_PREFIX x86_64-w64-mingw32)
 
 # cross compilers to use for C, C++ and Fortran
 set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc-posix)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++-posix)
 set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
+
+execute_process(
+    COMMAND ${CMAKE_C_COMPILER} -dumpmachine
+    OUTPUT_VARIABLE GCC_DUMPMACHINE
+)
+
+string(REGEX REPLACE "-.+$" "" CMAKE_SYSTEM_PROCESSOR "${GCC_DUMPMACHINE}")
 
 # target environment on the build host system
 set(CMAKE_FIND_ROOT_PATH /usr/${TOOLCHAIN_PREFIX})
